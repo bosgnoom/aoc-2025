14e103443ed8095309a41044efad9ca6c1ac5209
Paul Schouten <paul.schouten@gmail.com>
2025-12-01 13:52:36 +0100

    Added requirements.txt, tests for ending on zero

--- 01/test_part2.py
+++ 01/test_part2.py
@@ -14,7 +14,16 @@ def test_R1000():
     assert pos == 50
     assert clicks == 10
 
+def test_zero_R():
+    pos, clicks = part2.calc_pos(0, "R100")
+    assert pos == 0
+    assert clicks == 1
 
+def test_zero_L():
+    pos, clicks = part2.calc_pos(0, "L100")
+    assert pos == 0
+    assert clicks == 1
+    
 def test_sample1():
     assert part2.main(
         part1.read_input(Path('01/sample01.txt'))
